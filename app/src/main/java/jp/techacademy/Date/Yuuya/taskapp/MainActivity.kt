b2d2187db4c745d18f05c0ac7476b378be36288d
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-23 22:38:46 +0900

    mutable エラー

--- app/src/main/java/jp/techacademy/Date/Yuuya/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/Date/Yuuya/taskapp/MainActivity.kt
@@ -3,9 +3,20 @@ package jp.techacademy.Date.Yuuya.taskapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
+import io.realm.RealmChangeListener
+import io.realm.Sort
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mRealm: Realm
+    private val mRealmListener = object : RealmChangeListener<Realm> {
+        override fun onChange(element: Realm) {
+            reloadListView()
+        }
+    }
+
     private lateinit var mTaskAdapter: TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +27,10 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance()
+        mRealm.addChangeListener(mRealmListener)
 
         // ListViewの設定
         mTaskAdapter = TaskAdapter(this@MainActivity)
@@ -31,17 +46,40 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+        // アプリ起動時に表示テスト用のタスクを作成する
+        addTaskForTest()
+
         reloadListView()
     }
 
-    //aaaa
-
     private fun reloadListView() {
-        // 後でTaskクラスに変更する
-        val taskList = mutableListOf("aaa", "bbb", "ccc")
+        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
+        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
 
-        mTaskAdapter.taskList = taskList
+        // 上記の結果を、TaskList としてセットする
+        mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
+
+        // TaskのListView用のアダプタに渡す
         listView1.adapter = mTaskAdapter
+
+        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
         mTaskAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mRealm.close()
+    }
+
+    private fun addTaskForTest() {
+        val task = Task()
+        task.title = "作業"
+        task.contents = "プログラムを書いてPUSHする"
+        task.date = Date()
+        task.id = 0
+        mRealm.beginTransaction()
+        mRealm.copyToRealmOrUpdate(task)
+        mRealm.commitTransaction()
     }
 }
