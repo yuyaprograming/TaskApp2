770b2eb5b3bda42438a404e85b2d57e2131c6ff7
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-24 00:00:13 +0900

    6.3 inputxmlをandroidxにしてないから多分バグッてる

---
+++ app/src/main/java/jp/techacademy/Date/Yuuya/taskapp/InputActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.Date.Yuuya.taskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
